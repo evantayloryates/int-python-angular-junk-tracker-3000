556ad898ad512c252bd0bfab643272d494b75d69
Taylor Yates <evantayloryates@gmail.com>
2021-05-02 15:17:38 -0700

    Add general path structure for app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EmptyVehicleComponent } from './vehicles/empty-vehicle/empty-vehicle.component';
+import { UpdateVehicleComponent } from './vehicles/update-vehicle/update-vehicle.component';
+import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/vehicles', pathMatch: 'full' },
+    { path: 'vehicles/new/:type', component: UpdateVehicleComponent },
+    { path: 'vehicles/:id/edit', component: UpdateVehicleComponent},
+    { path: 'vehicles',component: AppComponent, children: [
+      { path: '', component: EmptyVehicleComponent },
+      { path: ':id', component: VehicleComponent }
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
