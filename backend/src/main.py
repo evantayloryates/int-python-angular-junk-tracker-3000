54929fe1aad7b3ba36ddea06b338f526517da84b
Taylor Yates <evantayloryates@gmail.com>
2021-05-02 16:10:23 -0700

    Add flask-cors to pipenv and implemented on app and all existing routes

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,17 +1,22 @@
 # coding=utf-8
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from database import Database
 from vehicle_registration_service import register_vehicle
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 database = Database()
 
 @app.route('/vehicles')
+@cross_origin()
 def get_vehicles():
   return jsonify(database.list())
 
 @app.route('/vehicles', methods=['POST'])
+@cross_origin()
 def create_vehicle():
   data = request.get_json()
   vehicle = database.create(data)
@@ -21,10 +26,12 @@ def create_vehicle():
   return (jsonify(vehicle), 201)
 
 @app.route('/vehicles/<id>')
+@cross_origin()
 def get_vehicle(id):
   return jsonify(database.find(int(id)))
 
 @app.route('/vehicles/<id>', methods=['PATCH'])
+@cross_origin()
 def update_vehicle(id):
   data = request.get_json()
   return jsonify(database.update(int(id), data))
