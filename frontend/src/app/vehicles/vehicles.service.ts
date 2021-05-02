9742ed5889aa248a85d9f54c2451d06548f02a41
Taylor Yates <evantayloryates@gmail.com>
2021-05-02 15:07:35 -0700

    Fix fn signature on deleteVehicle service function

--- frontend/src/app/vehicles/vehicles.service.ts
+++ frontend/src/app/vehicles/vehicles.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class VehiclesService {
 
-  constructor() { }
+  url = `${environment.BACKEND_URL}:${environment.BACKEND_PORT}`;
+
+  constructor(private http: HttpClient) { }
+
+
+  getVehicles() {
+    return this.http.get(`${this.url}/vehicles`);
+  }
+
+  postVehicle(vehicle) {
+    this.http.post(`${this.url}/vehicles`, {})
+  }
+
+  putVehicle(id, vehicle) {
+    this.http.put(`${this.url}/vehicles/${id}`, {})
+  }
+
+  deleteVehicle(id, vehicle) {
+    this.http.delete(`${this.url}/vehicles/${id}`)
+  }
 }
