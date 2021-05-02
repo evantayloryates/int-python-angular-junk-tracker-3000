4de213e76c0a6aa53d370923ed29949ecb3443de
Taylor Yates <evantayloryates@gmail.com>
2021-05-02 15:23:17 -0700

    Change root redirect to lead to Vehicles component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { AppComponent } from './app.component';
 import { EmptyVehicleComponent } from './vehicles/empty-vehicle/empty-vehicle.component';
 import { UpdateVehicleComponent } from './vehicles/update-vehicle/update-vehicle.component';
 import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/vehicles', pathMatch: 'full' },
     { path: 'vehicles/new/:type', component: UpdateVehicleComponent },
     { path: 'vehicles/:id/edit', component: UpdateVehicleComponent},
-    { path: 'vehicles',component: AppComponent, children: [
+    { path: 'vehicles',component: VehiclesComponent, children: [
       { path: '', component: EmptyVehicleComponent },
       { path: ':id', component: VehicleComponent }
     ]}
