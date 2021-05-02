cbd99edfa6b07337d1cf2aec83ab17ecc6bed8a0
Taylor Yates <evantayloryates@gmail.com>
2021-05-02 15:22:37 -0700

    Refactor broken imports and add vehicles component import

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { VehicleListComponent } from './vehicles/vehicle-list/vehicle-list.component';
+import { VehicleListItemComponent } from './vehicles/vehicle-list/vehicle-list-item/vehicle-list-item.component';
+import { EmptyVehicleComponent } from './vehicles/empty-vehicle/empty-vehicle.component';
+import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
+
+import { UpdateVehicleComponent } from './vehicles/update-vehicle/update-vehicle.component';
+import { UpdateSedanComponent } from './vehicles/update-vehicle/update-sedan/update-sedan.component';
 import { UpdateCoupeComponent } from './vehicles/update-vehicle/update-coupe/update-coupe.component';
 import { UpdateMinivanComponent } from './vehicles/update-vehicle/update-minivan/update-minivan.component';
 import { UpdateMotorcycleComponent } from './vehicles/update-vehicle/update-motorcycle/update-motorcycle.component';
-import { UpdateSedanComponent } from './vehicles/update-vehicle/update-sedan/update-sedan.component';
-import { VehicleListItemComponent } from './vehicles/vehicle-list/vehicle-list-item/vehicle-list-item.component';
-import { VehicleListComponent } from './vehicles/vehicle-list/vehicle-list.component';
-import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
 
 
 @NgModule({
@@ -19,10 +24,13 @@ import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
     VehicleListComponent,
     VehicleListItemComponent,
     VehicleComponent,
+    VehiclesComponent,
     UpdateCoupeComponent,
     UpdateSedanComponent,
     UpdateMinivanComponent,
-    UpdateMotorcycleComponent
+    UpdateMotorcycleComponent,
+    UpdateVehicleComponent,
+    EmptyVehicleComponent
   ],
   imports: [
     BrowserModule,
