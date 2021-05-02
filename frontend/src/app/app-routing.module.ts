a30cfd591604eaa0319bf4b9234a82788e3e576f
Taylor Yates <evantayloryates@gmail.com>
2021-05-02 16:23:04 -0700

    Update app routes to specify component for each new vehicle request

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+
 import { EmptyVehicleComponent } from './vehicles/empty-vehicle/empty-vehicle.component';
-import { UpdateVehicleComponent } from './vehicles/update-vehicle/update-vehicle.component';
 import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
 
+import { UpdateVehicleComponent } from './vehicles/update-vehicle/update-vehicle.component';
+import { UpdateCoupeComponent } from './vehicles/update-vehicle/update-coupe/update-coupe.component';
+import { UpdateMinivanComponent } from './vehicles/update-vehicle/update-minivan/update-minivan.component';
+import { UpdateMotorcycleComponent } from './vehicles/update-vehicle/update-motorcycle/update-motorcycle.component';
+import { UpdateSedanComponent } from './vehicles/update-vehicle/update-sedan/update-sedan.component';
+
+
 const appRoutes: Routes = [
     { path: '', redirectTo: '/vehicles', pathMatch: 'full' },
-    { path: 'vehicles/new/:type', component: UpdateVehicleComponent },
+    { path: 'vehicles/new/sedan', component: UpdateSedanComponent },
+    { path: 'vehicles/new/coupe', component: UpdateCoupeComponent },
+    { path: 'vehicles/new/minivan', component: UpdateMinivanComponent },
+    { path: 'vehicles/new/motorcycle', component: UpdateMotorcycleComponent },
     { path: 'vehicles/:id/edit', component: UpdateVehicleComponent},
     { path: 'vehicles',component: VehiclesComponent, children: [
       { path: '', component: EmptyVehicleComponent },
