e139b66f2b6f6346f98ce058ab0551073c84cd11
Taylor Yates <evantayloryates@gmail.com>
2021-05-02 18:22:29 -0700

    Add motorcycle and minivan update forms

--- frontend/src/app/vehicles/update-vehicle/update-minivan/update-minivan.component.ts
+++ frontend/src/app/vehicles/update-vehicle/update-minivan/update-minivan.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { VehiclesService } from '../../vehicles.service';
 
 @Component({
   selector: 'app-update-minivan',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateMinivanComponent implements OnInit {
 
-  constructor() { }
+  id: string;
 
+  minivanForm = new FormGroup({
+    nickname: new FormControl('', [Validators.required, Validators.maxLength(80)]),
+    mileage: new FormControl('', [Validators.required, Validators.min(0)]),
+    engine_status: new FormControl('works', [Validators.required]),
+    wheels: new FormControl('', [Validators.required, Validators.min(0), Validators.max(4)]),
+    doors: new FormControl('', [Validators.required, Validators.min(0), Validators.max(4)])
+  });
+
+  constructor(private router: Router,
+              private vehicleSrc: VehiclesService) { }
+
+  @Input() vehicle;
+  
   ngOnInit(): void {
+    if (this.vehicle) {
+      this.id = this.vehicle.id;
+      this.minivanForm.get('nickname').patchValue(this.vehicle["nickname"]);
+      this.minivanForm.get('mileage').patchValue(this.vehicle["mileage"]);
+      this.minivanForm.get('engine_status').patchValue(this.vehicle["engine_status"]);
+      this.minivanForm.get('wheels').patchValue(this.vehicle["wheels"]);
+      this.minivanForm.get('doors').patchValue(this.vehicle['doors'])
+    }
   }
 
+  getMileageRating(mileage: number): string {
+    if (mileage < 10000) return "low";
+    else if (mileage < 100000) return "medium";
+    else return "high";
+  }
+
+  onSubmit() {
+    if (this.minivanForm.valid) {
+      let form_vehicle = {
+        vehicle_type: 'minivan',
+        nickname: this.minivanForm.get('nickname').value,
+        mileage: this.minivanForm.get('mileage').value,
+        mileage_rating: this.getMileageRating(this.minivanForm.get('mileage').value),
+        engine_status: this.minivanForm.get('engine_status').value,
+        wheels: this.minivanForm.get('wheels').value,
+        doors: this.minivanForm.get('doors').value,
+      };
+      if (this.id) {
+        this.vehicleSrc.patchVehicle(this.id, form_vehicle).subscribe(
+          () => {
+            this.router.navigate(['vehicles']);
+          }
+        );
+      } else {
+        this.vehicleSrc.postVehicle(form_vehicle).subscribe(
+          () => {
+            this.router.navigate(['vehicles']);
+          });
+      }
+    }  
+  }
 }
+ 
--- frontend/src/app/vehicles/update-vehicle/update-motorcycle/update-motorcycle.component.ts
+++ frontend/src/app/vehicles/update-vehicle/update-motorcycle/update-motorcycle.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { VehiclesService } from '../../vehicles.service';
 
 @Component({
   selector: 'app-update-motorcycle',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateMotorcycleComponent implements OnInit {
 
-  constructor() { }
+  id: string;
 
+  motorcycleForm = new FormGroup({
+    nickname: new FormControl('', [Validators.required, Validators.maxLength(80)]),
+    mileage: new FormControl('', [Validators.required, Validators.min(0)]),
+    engine_status: new FormControl('works', [Validators.required]),
+    wheels: new FormControl('', [Validators.required, Validators.min(0), Validators.max(2)]),
+    seat_status: new FormControl('works', [Validators.required]),
+  });
+
+  constructor(private router: Router,
+              private vehicleSrc: VehiclesService) { }
+
+  @Input() vehicle;
+  
   ngOnInit(): void {
+    if (this.vehicle) {
+      this.id = this.vehicle.id;
+      this.motorcycleForm.get('nickname').patchValue(this.vehicle["nickname"]);
+      this.motorcycleForm.get('mileage').patchValue(this.vehicle["mileage"]);
+      this.motorcycleForm.get('engine_status').patchValue(this.vehicle["engine_status"]);
+      this.motorcycleForm.get('wheels').patchValue(this.vehicle["wheels"]);
+      this.motorcycleForm.get('seat_status').patchValue(this.vehicle['seat_status'])
+    }
   }
 
+  getMileageRating(mileage: number): string {
+    if (mileage < 10000) return "low";
+    else if (mileage < 100000) return "medium";
+    else return "high";
+  }
+
+  onSubmit() {
+    if (this.motorcycleForm.valid) {
+      let form_vehicle = {
+        vehicle_type: 'motorcycle',
+        nickname: this.motorcycleForm.get('nickname').value,
+        mileage: this.motorcycleForm.get('mileage').value,
+        mileage_rating: this.getMileageRating(this.motorcycleForm.get('mileage').value),
+        engine_status: this.motorcycleForm.get('engine_status').value,
+        wheels: this.motorcycleForm.get('wheels').value,
+        seat_status: this.motorcycleForm.get('seat_status').value,
+      };
+      if (this.id) {
+        this.vehicleSrc.patchVehicle(this.id, form_vehicle).subscribe(
+          patched_vehicle => {
+            this.router.navigate(['vehicles']);
+          }
+        );
+      } else {
+        this.vehicleSrc.postVehicle(form_vehicle).subscribe(
+          new_vehicle => {
+            this.router.navigate(['vehicles']);
+          });
+      }
+    }  
+  }
 }
+ 
