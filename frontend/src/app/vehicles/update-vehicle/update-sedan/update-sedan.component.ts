5d023a17e04db216015867331b0912ee900e2891
Taylor Yates <evantayloryates@gmail.com>
2021-05-02 18:14:31 -0700

    Remove unused import

--- frontend/src/app/vehicles/update-vehicle/update-sedan/update-sedan.component.ts
+++ frontend/src/app/vehicles/update-vehicle/update-sedan/update-sedan.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { VehiclesService } from '../../vehicles.service';
 
 @Component({
